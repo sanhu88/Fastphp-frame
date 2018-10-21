9008c84c729511b4915028aedff9ecbd6a6abf7e
Burt Zheng <504779282@qq.com>
2018-10-21 21:50:59 +0800

    add class_Fastphp to fastphp/fastphp.php

--- fastphp/Fastphp.php
+++ fastphp/Fastphp.php
@@ -3,3 +3,27 @@ namespace fastphp;
 
 // 框架根目录
 defined('CORE_PATH') or define('CORE_PATH', __DIR__);
+
+/**
+ * fastphp框架核心
+ */
+class Fastphp
+{
+    // 配置内容
+    protected $config = [];
+
+    public function __construct($config)
+    {
+        $this->config = $config;
+    }
+
+    // 运行程序
+    public function run()
+    {
+        spl_autoload_register(array($this, 'loadClass'));
+        $this->setReporting();
+        $this->removeMagicQuotes();
+        $this->unregisterGlobals();
+        $this->setDbConfig();
+        $this->route();
+    }
