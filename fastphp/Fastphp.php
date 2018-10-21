b41aaec74373693cf23ca6a08b3c2063f48db5e0
Burt Zheng <504779282@qq.com>
2018-10-21 22:14:08 +0800

    add class_Fastphp function route 1 to fastphp/fastphp.php

--- fastphp/Fastphp.php
+++ fastphp/Fastphp.php
@@ -27,3 +27,31 @@ class Fastphp
         $this->setDbConfig();
         $this->route();
     }
+    //路由处理
+    public function route()
+    {
+    	$controllerName = $this->config['defaultController'];
+    	$actionName = $this->['defaultAction'];
+    	$param = array();
+
+    	$url = $SERVER['REQUES_URI'];
+
+    	//清除？之后的部分
+    	$position = strpos($url, ?);
+    	$url = $position ===false ? $url : substr($url,0,$position);
+
+    	//删除前后的 ‘/’
+    	$url = trim($url,'/');
+
+    	if ($url){
+    		//使用‘/’分割字符串，并保存到数组
+    		$urlArray = explode('/',$url);
+    		//删除空数组元素
+    		$urlArray = array_filter($urlArray);
+    		//获取控制器名
+    		$controllerName = ucfirst($urlArray[0]);
+    		//获取动作名
+    		array_shift($urlArray);
+    		$param = $urlArray ? $urlArray : array();
+    	}
+    }
