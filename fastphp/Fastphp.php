43ef81f3bd3508967d0d69c2aac0bf19c6424cd0
Burt Zheng <504779282@qq.com>
2018-10-23 21:56:12 +0800

    finish function router to fastphp/fastphp.php

--- fastphp/Fastphp.php
+++ fastphp/Fastphp.php
@@ -54,4 +54,22 @@ class Fastphp
     		array_shift($urlArray);
     		$param = $urlArray ? $urlArray : array();
     	}
+
+    	//判断控制器是否存在
+    	$controller = 'app\\controllers\\'.$controllerName.'Controller';
+    	if (!class_exists($controller)) {
+    		exit($controller.'控制器不存在！')；
+    	}
+    	if (!method_exists($controller,$actionName)) {
+    		exit($actionName.'方法不存在')
+    	}
+    	// 如果控制器和操作名存在，则实例化控制器，因为控制器对象里面
+        // 还会用到控制器名和操作名，所以实例化的时候把他们俩的名称也
+        // 传进去。结合Controller基类一起看
+        $dispath = new $controller($controllerName,$actionName);
+
+
+        // $dispatch保存控制器实例化后的对象，我们就可以调用它的方法，
+        // 也可以像方法中传入参数，以下等同于：$dispatch->$actionName($param)
+        call_user_func_array(array($dispath, $actionName),$param);
     }
